7635b189f4eb96a5733115ded20aa50133460619
Ruán <ruan@murgatroyd.me>
2025-12-29 05:13:30 +0000

    Gotta fix that later

--- modules/apps/server/default.nix
+++ modules/apps/server/default.nix
@@ -2,14 +2,14 @@
   imports = map (f: ./${f}) (builtins.filter (f: f != "default.nix") (builtins.attrNames (builtins.readDir ./.)));
 
   # WireGuard
-  age.secrets.stickyConfig.file = "/etc/nixos/secrets/${config.networking.hostName}Config.age";
-  networking.wg-quick.interfaces.opusNet.configFile = config.age.secrets.stickyConfig.path;
+  # age.secrets.stickyConfig.file = "/etc/nixos/secrets/${config.networking.hostName}Config.age";
+  # networking.wg-quick.interfaces.opusNet.configFile = config.age.secrets.stickyConfig.path;
 
-  age.secrets.gitKey = {
-    file = /etc/nixos/secrets/gitFile.age;
-    path = "/home/rjm/.ssh/git_key";
-    owner = "rjm";
-    group = "users";
-    mode = "600";
-  };
+  # age.secrets.gitKey = {
+  #   file = /etc/nixos/secrets/gitFile.age;
+  #   path = "/home/rjm/.ssh/git_key";
+  #   owner = "rjm";
+  #   group = "users";
+  #   mode = "600";
+  # };
 }

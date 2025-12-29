7a1614b09a7bd84b14609a3baa741863ff1cc40f
Ruán <ruan@murgatroyd.me>
2025-12-29 05:07:16 +0000

    Huh

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,7 +1,7 @@
 let
   rjm = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKizxhkpIN/7+bQShmOhq8hJLOzRXGhkOGdfgMbCEFcZ";
   root = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDQpf9nMOyXwqTQRogphnel0HIK5Eqip8Owzu7xgZ/qO root@nixos"
-  users = [ rjm ];
+  users = [ rjm root ];
 
   daniil = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHeLqxshroeY9js/xM/RqL+PDydqwEALHzOv7PrBvdzZ";
   sticky = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBpJPN19Jelx3yE/P7M3XU5NW0YDHV3RYg89HeqZDQku";
